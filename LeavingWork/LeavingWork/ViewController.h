28813ffd078e2a8934c48ee6558b0972036957c6
georgestrajan <George@Georges-Mac-mini.local>
2014-07-30 21:38:52 -0700

    First commit

---
+++ LeavingWork/LeavingWork/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LeavingWork
+//
+//  Created by George Strajan on 7/30/14.
+//  Copyright (c) 2014 GS Industries LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ LeavingWork/LeavingWork/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  LeavingWork
+//
+//  Created by George Strajan on 7/30/14.
+//  Copyright (c) 2014 GS Industries LLC. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+            
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
